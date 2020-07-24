3cae208cea80417526571e7b3054199eefa096ae
DarkOnion0 <Darkgenius1@protonmail.com>
2020-07-24 21:18:21 +0200

    update moyenne-console.py

--- moyenne.py
+++ moyenne.py
@@ -4,17 +4,28 @@ stop = False
 tmp = []
 tmpn = ""
 tmpc = ""
-while stop != True :
-    # Note
-    print("Entrer une note")
-    tmpn = input(">>> ")
-    # Coefficiant
-    print("Entrer un coef")
-    tmpc = input(">>>")
-    # Merging
-    tmpn=str(tmpn)
-    tmpc=int(tmpc)
-    tmp.append((tmpn, tmpc))
+flag1 = 0
+while stop == False :
+    while flag1 == 0 :
+        # Note
+        print("Entrer une note")
+        tmpn = input(">>> ")
     
-    print(tmp)
-    stop = True
+        # Coefficiant
+        print("Entrer un coef")
+        tmpc = input(">>>")
+    
+        # Merging
+        try:
+            tmpn = float(tmpn)
+            tmpc = float(tmpc)
+            tmp.append((tmpn, tmpc))
+            print(tmp)
+            flag1 == 1
+        except ValueError :
+            print("Veuillez Insérer un nombre")
+
+    print("Voulez vous continuer (o/n) ?")
+    stoptmp = input(">>> ")
+    if stoptmp == "n" :
+        stop = True
